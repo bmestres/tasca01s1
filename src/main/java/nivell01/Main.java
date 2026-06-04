c7fa423850a5bdb8539cc145d6765c35fe8057ba
Bernat Mestres <ber.mestres@gmail.com>
2026-06-04 12:07:42 +0200

    feat: add Main class with main() method

---
+++ src/main/java/nivell01/Main.java
@@ -0,0 +1,19 @@
+package nivell01;
+
+public class Main {
+
+    static void main(String[] args) {
+
+        // Create new Wind Instrument
+        WindInstrument instrument01 = new WindInstrument("Horn", 1338.5);
+        // Create new String Instrument
+        StringInstrument instrument02 = new StringInstrument("Ones Martenot", 4830.75);
+        // Create new Percussion Instrument
+        PercussionInstrument instrument03 = new PercussionInstrument("Marimba",3248.2 );
+
+        // Call instrument's play action
+        instrument01.play();
+        instrument02.play();
+        instrument03.play();
+    }
+}
