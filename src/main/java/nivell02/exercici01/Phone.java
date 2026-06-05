03991253cc6da86ee1024063ccb71f0607dfd541
Bernat Mestres <ber.mestres@gmail.com>
2026-06-05 12:39:46 +0200

    Feat: Phone class polished

--- src/main/java/nivell02/exercici01/Phone.java
+++ src/main/java/nivell02/exercici01/Phone.java
@@ -1,7 +1,42 @@
 package nivell02.exercici01;
-
+// Create superclass Phone
 public class Phone {
     // Define attributes
-    private static final String brand;
+    private static String brand;
     private static String model;
+
+    // Constructor
+    public Phone(String brand, String model){
+
+        // Check input data
+        checkInputs(brand, model);
+
+        // Initialize attributes
+        this.brand = brand;
+        this.model = model;
+    }
+
+    // Check inputs method
+    private void checkInputs(String brand, String model){
+        if(brand == null)throw new IllegalArgumentException("Brand must not be NULL");
+        if(model == null)throw new IllegalArgumentException("Model must not be NULL");
+    }
+
+    // Setters
+    public void setBrand(String brand){
+        this.brand = brand;
+    }
+
+    public void setModel(String model){
+        this.model = model;
+    }
+
+    // Getters
+    public String getBrand(){
+        return this.brand;
+    }
+
+    public String getModel(){
+        return this.model;
+    }
 }
---
+++ src/main/java/nivell02/exercici01/Smartphone.java
@@ -0,0 +1,11 @@
+package nivell02.exercici01;
+// Create subclass Smartphone
+public class Smartphone extends Phone{
+
+    // Constructor
+    public Smartphone(String brand, String model){
+
+        // Initialize superclass
+        super(brand, model);
+    }
+}
