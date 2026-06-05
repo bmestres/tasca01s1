46f626a715fe78d95d9e3fab67770a5f9156d92a
Bernat Mestres <ber.mestres@gmail.com>
2026-06-05 19:30:31 +0200

    Feature: Redactor class created

--- src/main/java/nivell03/exercici01/Redactor.java
+++ src/main/java/nivell03/exercici01/Redactor.java
@@ -1,5 +1,19 @@
 package nivell03.exercici01;
 
+import java.util.ArrayList;
+
 public class Redactor {
 
+    // Declare variables
+    private String nom;
+    private final String nif;
+    private static double sou = 1500;
+    private ArrayList<Noticia> noticies;
+
+    // Constructor
+    public Redactor(String nom, String nif){
+        this.nom = nom;
+        this.nif = nif;
+        this.noticies = new ArrayList<>();
+    }
 }
