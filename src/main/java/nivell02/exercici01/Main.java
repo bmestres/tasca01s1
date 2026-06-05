fb433f179baefed5accd635184b917c0e2c90f3f
Bernat Mestres <ber.mestres@gmail.com>
2026-06-05 13:49:42 +0200

    Feat: Main class implemented and tested

--- src/main/java/nivell02/exercici01/Main.java
+++ src/main/java/nivell02/exercici01/Main.java
@@ -5,7 +5,14 @@ public class Main {
     static void main(String[] args) {
 
         Smartphone smartphone01 = new Smartphone("iPhone", "17");
-
+        // Call calling method
         smartphone01.makeCall("6557893");
+
+        // Call take picture method
+        smartphone01.takePicture();
+
+        // Call ring alarm method
+        smartphone01.ringAlarm();
+
     }
 }
